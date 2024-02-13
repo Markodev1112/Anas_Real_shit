351acb7c28fd0d23cedf085a4bdd27ff67f43bb6
kevin <zeradhenny474@gmail.com>
2024-02-13 16:29:22 -0500

    make same size images and same line price on product

---
+++ assets/custom.js
@@ -0,0 +1,7 @@
+document.querySelector('.custom-search__input').addEventListener('keydown', function(){
+    console.log("search");
+    document.querySelector('.search__button').css('display', 'none')
+// document.querySelector('.show_search').addEventListener('mouseleave', function() {
+//     // Your code to execute when the mouse leaves the element
+//     console.log('Mouse left the element!');
+});
