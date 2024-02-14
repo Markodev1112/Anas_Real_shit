73eb5e6596d29e47ad95dbbddae5aecca9788800
kevin <zeradhenny474@gmail.com>
2024-02-14 10:57:07 -0500

    remove flickity slider and back up to first slider

--- assets/custom.js
+++ assets/custom.js
@@ -5,3 +5,10 @@ document.querySelector('.custom-search__input').addEventListener('keydown', func
 //     // Your code to execute when the mouse leaves the element
 //     console.log('Mouse left the element!');
 });
+
+
+var flkty = new Flickity('.carousel', {
+  // Set the speed of transitions
+  selectedAttraction: 0.01, // Adjust the attraction speed
+  friction: 0.15 // Adjust the friction/damping effect
+});
