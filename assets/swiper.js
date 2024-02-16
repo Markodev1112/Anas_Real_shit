106680db68d30c928fe39a301ebe1e01c37edbf3
kevin <zeradhenny474@gmail.com>
2024-02-16 17:00:05 -0500

    finish slider speed and cart product style

---
+++ assets/swiper.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 2,
+  centeredSlides: false,
+  spaceBetween: 0,
+  grabCursor: true,
+  preventClicks: true,
+  freeMode: true,
+  freeModeSticky: true,
+  slideToClickedSlide: true,
+  speed: 500,
+  freeModeMomentumRatio: 0.3,
+  freeModeMomentumVelocityRatio: 1,
+});
+
+// When changin the slidesPerView, also update HTML class modifier
+
+
+
+
+
