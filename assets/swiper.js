81dcb4439528e6ad71b1431499d2871328b03fa3
kevin <zeradhenny474@gmail.com>
2024-02-16 17:45:23 -0500

    finish slider header menu

--- assets/swiper.js
+++ assets/swiper.js
@@ -15,6 +15,25 @@ var swiper = new Swiper(".swiper-container", {
 // When changin the slidesPerView, also update HTML class modifier
 
 
+var swiper = new Swiper(".swiper-header-container", {
+  slidesPerView: 4,
+  centeredSlides: false,
+  spaceBetween: 10,
+  grabCursor: true,
+  preventClicks: true,
+  slideToClickedSlide: true,
+  speed: 500,
+  freeModeMomentumRatio: 0.3,
+  freeModeMomentumVelocityRatio: 1,
+  loop: true
+});
+
+// When changin the slidesPerView, also update HTML class modifier
+
+
+
+
+
 
 
 
