135ba0feb86f8688d92d486722cba67d33faa2e4
marko <marko746172@gmail.com>
2024-02-21 09:57:25 +0100

    add transition effect on mobile header

--- assets/custom.js
+++ assets/custom.js
@@ -1,27 +1,29 @@
-
-
 // Check if the user is on a mobile device
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     var lastScrollTop = 0;
+    window.addEventListener("scroll", function(){
+        var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+        this.document.querySelectorAll('.custom-mobile-setting').forEach(item => {
+            if (currentScroll > lastScrollTop){    
+                    // Scroll down
+                    item.style.display="none";
+                    this.document.querySelector('.custom-header').classList.remove('show');
+                    this.document.querySelector('.custom-header').classList.add('hidden');
+            } else {
+                // Scroll up            
+                    item.style.display="none";
+                    this.document.querySelector('.custom-header').classList.remove('show');
+                    this.document.querySelector('.custom-header').classList.add('hidden');    
+                    if (currentScroll == 0) {
+                        item.style.display="block";
+                        this.document.querySelector('.custom-header').classList.add('show');
+                        this.document.querySelector('.custom-header').classList.remove('hidden');
+                    }    
+            }
 
-window.addEventListener("scroll", function(){
-   var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-   if (currentScroll > lastScrollTop){
-       console.log("down");
-   } else {
-       // Scroll up
-       console.log(currentScroll, "up");
-       
-       this.document.querySelector('.scroll-display').style.display = "none";
-
-        if (currentScroll == 0) {
-        this.document.querySelector('.scroll-display').style.display = "block";
-        console.log("0");
-        }
-
-   }
-   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
-}, false);
+            lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+        })
+    }, false);
     console.log("User is on a mobile device.");
 } else {
     console.log("User is not on a mobile device.");
