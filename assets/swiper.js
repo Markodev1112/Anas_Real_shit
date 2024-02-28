4e95a16c278ad4cbd1803ad28fd9b0836f28a5f1
marko <marko746172@gmail.com>
2024-02-28 19:32:26 +0100

    customized price and circle collection slider

--- assets/swiper.js
+++ assets/swiper.js
@@ -20,7 +20,8 @@ var swiper = new Swiper(".swiper-container", {
 }
 });
 
-// When changin the slidesPerView, also update HTML class modifier
+
+
 
 
 var swiper = new Swiper(".swiper-header-container", {
@@ -36,7 +37,54 @@ var swiper = new Swiper(".swiper-header-container", {
   loop: true
 });
 
-// When changin the slidesPerView, also update HTML class modifier
+// Featured-collection on mobile
+
+var swiper = new Swiper(".swiper-container-list", {
+  slidesPerView: 2,
+  centeredSlides: false,
+  spaceBetween: 10,
+  grabCursor: true,
+  preventClicks: true,
+  freeMode: true,
+  freeModeSticky: true,
+  slideToClickedSlide: true,
+  speed: 1500,
+  freeModeMomentumRatio: 0.3,
+  freeModeMomentumVelocityRatio: 1,
+  // centeredSlidesBounds: true,
+  on: {
+	snapGridLengthChange:function(){
+		if( this.snapGrid.length != this.slidesGrid.length ){
+			this.snapGrid = this.slidesGrid.slice(1);
+		}
+	}
+}
+});
+
+// Featured-collection on desktop
+
+var swiper = new Swiper(".swiper-container-list-desktop", {
+  slidesPerView: 4,
+  centeredSlides: false,
+  spaceBetween: 20,
+  grabCursor: true,
+  preventClicks: true,
+  freeMode: true,
+  freeModeSticky: true,
+  slideToClickedSlide: true,
+  speed: 1500,
+  freeModeMomentumRatio: 0.3,
+  freeModeMomentumVelocityRatio: 1,
+  // centeredSlidesBounds: true,
+  on: {
+	snapGridLengthChange:function(){
+		if( this.snapGrid.length != this.slidesGrid.length ){
+			this.snapGrid = this.slidesGrid.slice(3);
+		}
+	}
+}
+});
+
 
 
 
